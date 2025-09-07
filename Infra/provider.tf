3f5fef0e2708019ded0d3b04806591687acc236d
Ajay Kumar <ajay.kr931@gmail.com>
2025-09-08 00:06:01 +0530

    Update provider.tf

--- Infra/provider.tf
+++ Infra/provider.tf
@@ -5,15 +5,15 @@ terraform {
       version = "4.38.1"
     }
   }
-  # backend "azurerm" {
-  #   use_cli              = true                                    # Can also be set via `ARM_USE_CLI` environment variable.
-  #   tenant_id            = "e30abb13-e248-442c-b835-29af747e86b6"  # Can also be set via `ARM_TENANT_ID` environment variable. Azure CLI will fallback to use the connected tenant ID if not supplied.
-  #   subscription_id      = "ecf1c36f-8eba-469e-841f-a595c299e914"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable. Azure CLI will fallback to use the connected subscription ID if not supplied.
-  #   resource_group_name  = "myChildResourceGroup"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-  #   storage_account_name = "mystorageaccount129"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-  #   container_name       = "tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-  #   key                  = "backend.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  # }
+    backend "azurerm" {
+  use_cli              = true                                    # Can also be set via `ARM_USE_CLI` environment variable.
+  tenant_id            = "e30abb13-e248-442c-b835-29af747e86b6"  # Can also be set via `ARM_TENANT_ID` environment variable. Azure CLI will fallback to use the connected tenant ID if not supplied.
+  subscription_id      = "ecf1c36f-8eba-469e-841f-a595c299e914"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable. Azure CLI will fallback to use the connected subscription ID if not supplied.
+  resource_group_name  = "Autodep-RG"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+  storage_account_name = "autodepstg"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+  container_name       = "tfcontainer"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+  key                  = "backend.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+   }
 }
 
 provider "azurerm" {
